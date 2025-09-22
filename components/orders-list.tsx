178c899e7ef6c716e6c00165cb57066efbce1388
clownlessmode <clownessmode@bk.ru>
2025-09-22 10:54:10 +0700

    fix

--- components/orders-list.tsx
+++ components/orders-list.tsx
@@ -91,7 +91,7 @@ export function OrdersList({ orders, onSelectOrder }: OrdersListProps) {
           onClick={() => onSelectOrder(order)}
         >
           <div className="flex items-center justify-between ">
-            <h2 className="text-3xl font-bold text-foreground">
+            <h2 className="text-xl font-bold text-foreground">
               Заказ {order.number}
             </h2>
             <Badge
